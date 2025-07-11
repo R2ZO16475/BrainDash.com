94c63cebdaff33b7f904122161eb69b6ebc4c7a4
R2ZO16475 <sarah.e.artusio@gmail.com>
2025-07-11 15:10:42 -0400

    Create server.js
    Signed-off-by: R2ZO16475 <sarah.e.artusio@gmail.com>

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 4000;
+
+app.use(express.static('public')); 
+
+io.on('connection', (socket) => {
+  console.log('a user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id);
+  });
+
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
